55a48ee138c2096f6f4c1b5084c8821824a2da0b
Shivaraj-star-cloud <shivanepalme@yahoo.com>
2023-06-03 22:25:04 -0700

    Face--Look

---
+++ core/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name='like-post'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings'),
+    path('home', views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+
+
+]
